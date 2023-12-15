f9627023544c0bac60fddde779a763891f51dd20
ERASE AI Research <153342374+eraseai@users.noreply.github.com>
2023-12-15 22:10:20 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 source conda activate
-conda create -n erase python=3.9
+conda create -n erase python=3.8
 conda activate erase 
 
 pip install torch ==2.0.0
@@ -8,7 +8,7 @@ pip install torch-geometric==2.3.1
 pip install matplotlib==3.7.1
 pip install scikit-learn==1.3.0
 pip install scipy==1.10.1
-conda create -n rapids-22.12 -c rapidsai -c conda-forge -c nvidia cuml=22.12 python=3.9 cudatoolkit=11.8
+conda create -n rapids-23.4.1 -c rapidsai -c conda-forge -c nvidia cuml=23.4.1 python=3.8 cudatoolkit=11.8
 pip install torch-scatter -f https://pytorch-geometric.com/whl/torch-2.0.0+cu118.html
 pip install torch-sparse -f https://pytorch-geometric.com/whl/torch-2.0.0+cu118.html
 pip install torch-cluster -f https://pytorch-geometric.com/whl/torch-2.0.0+cu118.html
