6733aeaf6d903463140f497fd897c0cb3f41a5da
ERASE AI Research <153342374+eraseai@users.noreply.github.com>
2023-12-15 22:07:49 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,9 +8,7 @@ pip install torch-geometric==2.3.1
 pip install matplotlib==3.7.1
 pip install scikit-learn==1.3.0
 pip install scipy==1.10.1
-pip install \
-    --extra-index-url=https://pypi.nvidia.com \
-    cudf-cu11==23.12.*  cuml-cu11==23.12.* 
+conda create -n rapids-22.12 -c rapidsai -c conda-forge -c nvidia cuml=22.12 python=3.9 cudatoolkit=11.8
 pip install torch-scatter -f https://pytorch-geometric.com/whl/torch-2.0.0+cu118.html
 pip install torch-sparse -f https://pytorch-geometric.com/whl/torch-2.0.0+cu118.html
 pip install torch-cluster -f https://pytorch-geometric.com/whl/torch-2.0.0+cu118.html
