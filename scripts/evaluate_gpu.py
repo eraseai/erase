1b41dd2db4695d4de6590f521c811584527d1ba6
youngsoul0731 <1917738522@qq.com>
2023-12-15 16:01:07 +0800

    update

--- scripts/evaluate_gpu.py
+++ scripts/evaluate_gpu.py
@@ -24,31 +24,14 @@ from cuml.preprocessing import normalize
 from cuml.linear_model import LogisticRegression
 
 
-def Linear_classifier(out,split_idx,noisy_train_labels,clean_labels,evaluator):
-    """
-    Evaluate the representation quality of the model using logistic regression.
-
-    Args:
-        out: Learned representations.
-        split_idx: The split index.
-        clean_labels: Ground-truth node labels.
-        evaluator: The evaluator from ogb.nodeproppred.
-
-    Returns:
-        train_acc: Accuracy on training set.
-        valid_acc: Accuracy on validation set.
-        test_acc: Accuracy on test set.
-    """
-    #detach the output from the graph and normalize it
+def Linear_classifier(args, out,split_idx,noisy_train_labels,clean_labels,evaluator):
+    device = args.device
+    num_classes = (torch.max(clean_labels)+1).cpu().item()
     out = out.detach().cpu().numpy()
     out = normalize(out,norm='l2')
-
-    #train a logistic regression classifier using the representations
     train_features = out[split_idx['train']]
     clf = LogisticRegression(solver='qn', max_iter=500).fit(train_features, noisy_train_labels.ravel())
     y_pred = clf.predict_proba(out).argmax(axis=-1, keepdims=True) 
-
-    #use the evaluator from ogb.nodeproppred to compute the accuracy of the classifier in order to measure the representation quality
     train_acc = evaluator.eval({
         'y_true': clean_labels[split_idx['train']],
         'y_pred': y_pred[split_idx['train']],
